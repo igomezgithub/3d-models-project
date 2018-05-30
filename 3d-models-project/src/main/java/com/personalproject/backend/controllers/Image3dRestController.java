0a98e26e26fd6b00b4c445966529dea6a48a8056
Iván Gómez <ivangomrod@gmail.com>
2018-05-30 20:09:29 +0200

    [0005] [Infrastructure] [Back-end] Added CROSS support

--- 3d-models-project/src/main/java/com/personalproject/backend/controllers/Image3dRestController.java
+++ 3d-models-project/src/main/java/com/personalproject/backend/controllers/Image3dRestController.java
@@ -3,6 +3,7 @@ package com.personalproject.backend.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.personalproject.backend.models.entity.Image3d;
 import com.personalproject.backend.models.services.IImage3dService;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 public class Image3dRestController {
